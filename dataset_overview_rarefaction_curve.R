d543f752ae6a9de437e66973eb50954fe0e5afdb
kkyume <kzkliu326@gmail.com>
2025-10-27 23:44:28 -0800

    Updated rarefaction curve to be publication-ready

--- dataset_overview_rarefaction_curve.R
+++ dataset_overview_rarefaction_curve.R
@@ -5,6 +5,6 @@ library(vegan)
 counts = read.delim("feature-table.tsv", header = TRUE, sep = "\t", skip = 1, row.names=1)
 
 # Generate rarefaction curve
-rarecurve(t(counts), step = 1000)
+rarecurve(t(data.frame(counts)), step = 1000)
 
 
